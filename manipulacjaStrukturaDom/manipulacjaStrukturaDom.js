335d7bf9422260486a6e69a40db5a391915fc75b
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-11 11:33:47 +0200

    manipulacjaStruktura

--- manipulacjaStrukturaDom/manipulacjaStrukturaDom.js
+++ manipulacjaStrukturaDom/manipulacjaStrukturaDom.js
@@ -1,2 +1,20 @@
 'use strict';
 
+var mainHeader = document.getElementById('main-header');
+
+console.log(mainHeader);
+
+mainHeader.innerHTML = "tresc naglowka";
+
+var link = document.getElementsByClassName('link')[0];
+
+console.log(link);
+
+link.href = "https://akademia108.pl";
+
+link.className = "nowa-klasa";
+
+mainHeader.style.color = "#11aa22";
+
+link.text = "dodany tekst";
+
