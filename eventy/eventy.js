15907b69c4c62c8ec73c010e13fc6a592ed3283d
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-11 13:06:46 +0200

    resize mouseover

--- eventy/eventy.js
+++ eventy/eventy.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var secondLink = document.getElementsByTagName('a')[1];
+/*var secondLink = document.getElementsByTagName('a')[1];
 
 console.log(secondLink);
 
@@ -12,3 +12,24 @@ function alarm(event) {
 }
 
 secondLink.onclick = alarm;
+
+
+var thirdLink = document.getElementsByTagName('a')[2];
+
+thirdLink.addEventListener('click', alarm);*/
+
+var mainHeader = document.getElementById('main-header');
+
+function resize(e) {
+    if (e.type == 'mouseover') {
+        mainHeader.style.fontSize = "70px";
+    }
+    
+    else {
+        mainHeader.style.fontSize = "inherit";
+    }
+}
+
+mainHeader.onmouseover = resize;
+
+mainHeader.onmouseout = resize;
