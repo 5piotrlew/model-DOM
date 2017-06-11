537978c4f39b614cb64430b956140b8024a6296a
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-11 10:00:13 +0200

    get element by id

---
+++ DOM/DOM.js
@@ -0,0 +1,15 @@
+'use strict';
+
+console.log (window);
+console.log (window.innerHeight);
+console.log (window.outerHeight);
+
+
+console.log('document');
+console.log(document);
+console.log('documentElement');
+console.log(document.documentElement);
+console.log('head');
+console.log(document.head);
+console.log('body');
+console.log(document.body);
